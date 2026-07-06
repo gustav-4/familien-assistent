a08279c70a999b44a3cf7945db36ff6dc2d5d332
gustav-4 <info@gustav-4.de>
2026-07-06 18:27:46 +0200

    Delete familien_assistent_ARBEIT directory

--- familien_assistent_ARBEIT/START_ASSISTENT.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Familien-Assistent - oeffnet immer die ONLINE-Version.
-rem (Lokal per Doppelklick fehlen Server-Funktionen wie die
-rem  Rezept-Recherche und der Erinnerungs-Server.)
-start https://familienassistent.net/
