e4a758de117d395e1abd55b96fb10323d28b5f3d
gustav-4 <info@gustav-4.de>
2026-07-29 15:48:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * dieses Strings reicht, damit der Browser die sw.js als
  * geändert erkennt und den Update-Zyklus (Weg A) auslöst.
  */
-const VERSION = "app-fusion22";
+const VERSION = "app-fusion24";
 
 self.addEventListener("install", (e) => self.skipWaiting());
 
